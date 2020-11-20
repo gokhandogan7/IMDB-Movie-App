98ad265d8feaabac585b7087aa0a955064f39457
gokhandogan7 <gdogan3746@gmail.com>
2020-11-20 13:06:19 +0100

    Componets created

---
+++ src/components/Card/Card.style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledCardWrapper = styled.div`
+width: 100px;
+height:240px;
+color:#bdbdbd;
+box-shadow: 3px 2px 10px rgba(0, 0, 0, 0.5);
+
+`
---
+++ src/components/Card/index.js
@@ -0,0 +1,12 @@
+import {StyledCardWrapper} from './Card.style'
+
+
+
+export const Card = ()=>{
+    return(
+    <StyledCardWrapper>Card</StyledCardWrapper>
+        )
+    
+}
+
+
